67985c81a2b1ab6e547586707bae2cb84d1c6f84
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2026-01-05 11:35:53 -0800

    parse var, do tricks with them

--- Blender_CUT_IO/cutimportparser.py
+++ Blender_CUT_IO/cutimportparser.py
@@ -13,7 +13,7 @@ def cut_importV2_parser(f):
     ob = bpy.context.object
 
     idx=0
-    version = unpack("<I", f.read(4))[0]
+    Version = unpack("<I", f.read(4))[0]
     if Version == 1:
         EntrySize1 = unpack("<H", f.read(2))[0]
         id1 = unpack("<H", f.read(2))[0]
@@ -45,4 +45,21 @@ def cut_importV2_parser(f):
         bpy.context.scene.frame_end = int(FloatCount)
         bpy.context.scene.frame_current = 1
         bpy.context.scene.frame_start = 1
+
+def cut_exportV2_parser(f):
+    
+    ob = bpy.context.object
+
+    idx=0
+    if Version == 1:
+        f.write(pack("<I", Version-Version+1))
+        f.write(pack("<H", EntrySize1-EntrySize1+32))
+        f.write(pack("<H", id1-id1))
+        f.write(pack("<f", FloatCount-FloatCount+bpy.context.scene.frame_end))
+        
+    elif Version == 2:
+        f.write(pack("<I", Version-Version+2))
+        f.write(pack("<H", EntrySize1-EntrySize1+32))
+        f.write(pack("<H", id1-id1))
+        f.write(pack("<f", FloatCount-FloatCount+bpy.context.scene.frame_end))
         
